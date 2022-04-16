3fc515b1c5e4c25142dcaeea224e971cfcc19a07
justinwkcheung <justin.wk.cheung@gmail.com>
2022-04-15 17:22:58 -0700

    Add photo credits section with heart asset, change background to be stored as a json object and use try catch to parse the json, reference source from background object for photo creds, fix styling to be more small monitor friendly

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import IconHeart from "../../static/icons/heart.png";
+
 const defaultBackground = "https://images.unsplash.com/photo-1546529249-8de036dd3c9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
 
 class Background extends Component {
@@ -9,7 +11,9 @@ class Background extends Component {
 
     this.state = {
       backgroundUrl: "",
+      backgroundSource: "",
       isToday: this.isToday(),
+      showCreditText: false,
     };
   }
 
@@ -19,9 +23,23 @@ class Background extends Component {
 
   setBackgroundImage = () => {
     if (this.state.isToday) {
-      const background = localStorage.getItem('allmydays_backgroundToday');
+      let background;
+      const backgroundData = localStorage.getItem('allmydays_backgroundToday');
+
+      try {
+        background = JSON.parse(backgroundData);
+      } catch (err) {
+        if (typeof backgroundData == 'string') {
+          background = backgroundData;
+        }
+      }
+
       if (background) {
-        this.setState({ backgroundUrl: background });
+        this.setState({
+          // remove OR statements next release
+          backgroundUrl: background.url || background,
+          backgroundSource: background.source || ""
+        });
         return true;
       }
     }
@@ -29,13 +47,22 @@ class Background extends Component {
     this.getBackgroundImage().then((backgroundData) => {
       this.setBackgroundLocalInfo(backgroundData);
       let backgroundToSet = defaultBackground;
+      let backgroundSource = this.state.backgroundSource;
 
       if (backgroundData[0].data) {
         backgroundToSet = backgroundData[0].data.attributes.file_url;
+        backgroundSource = backgroundData[0].data.attributes.source;
       }
 
-      this.setState({ backgroundUrl: backgroundToSet });
+      this.setState({
+        backgroundUrl: backgroundToSet,
+        backgroundSource: backgroundSource
+      });
     });
+  }
+
+  toggleShowCreditText = () => {
+    this.setState({ showCreditText: !this.state.showCreditText });
   }
 
   getBackgroundImage = () => {
@@ -58,11 +85,43 @@ class Background extends Component {
   setBackgroundLocalInfo = (backgroundData) => {
     localStorage.allmydays_date = new Date().toLocaleDateString();
     if (backgroundData[0].data) {
-      localStorage.allmydays_backgroundToday = backgroundData[0].data.attributes.file_url;
+      localStorage.allmydays_backgroundToday =
+      JSON.stringify({
+        url: backgroundData[0].data.attributes.file_url,
+        source: backgroundData[0].data.attributes.source
+      });
     }
     if (backgroundData[1].data) {
-      localStorage.allmydays_backgroundTomorrow = backgroundData[1].data.attributes.file_url;
+      localStorage.allmydays_backgroundTomorrow =
+      JSON.stringify({
+        url: backgroundData[1].data.attributes.file_url,
+        source: backgroundData[1].data.attributes.source
+      });
     }
+  }
+
+  creditTextClass = () => {
+    if (this.state.showCreditText) {
+      return "credit-text seen";
+    }
+
+    return "credit-text";
+  }
+
+  renderCreditSection = () => {
+    if (this.state.backgroundSource) {
+      return (
+        <section className="photo-credit-section"
+          onMouseOver={this.toggleShowCreditText}
+          onMouseOut={this.toggleShowCreditText}
+        >
+          <img src={IconHeart} alt={"heart"}></img>
+          <p className={this.creditTextClass()}>Photo By {this.state.backgroundSource}</p>
+        </section>
+      );
+    }
+
+    return null;
   }
 
   render() {
@@ -74,6 +133,7 @@ class Background extends Component {
           url("${this.state.backgroundUrl}")`
         }}
       >
+        {this.renderCreditSection()}
       </div>
     )
   }
